1292cbdaf6222455ce8ef517f3a62c1f4f867097
Karl <karl.hepworth@gmail.com>
2019-09-30 09:31:07 +1000

    Add GitLab CI support

--- .docker/push.sh
+++ .docker/push.sh
@@ -11,35 +11,40 @@ IMAGE_VERSION_TAG=${IMAGE_VERSION_TAG:-}
 # prevent stripping of the version.
 IMAGE_VERSION_TAG_PREFIX=${IMAGE_VERSION_TAG_PREFIX:-7.x-}
 # Docker image edge tag.
-IMAGE_TAG_EDGE=${IMAGE_TAG_EDGE:-beta}
+IMAGE_TAG_EDGE=${IMAGE_TAG_EDGE:-}
 # Flag to force image build.
 FORCE_IMAGE_BUILD=${FORCE_IMAGE_BUILD:-}
 # Path prefix to Dockerfiles extension that is used as a name of the service.
 FILE_EXTENSION_PREFIX=${FILE_EXTENSION_PREFIX:-.docker/Dockerfile.}
+# CLI Image name
+CLI_IMAGE=${DOCKERHUB_NAMESPACE:-govcmslagoon}/govcms
 
 for file in $(echo $FILE_EXTENSION_PREFIX"*"); do
     service=${file/$FILE_EXTENSION_PREFIX/}
 
     version_tag=$IMAGE_VERSION_TAG
-    [ "$IMAGE_VERSION_TAG_PREFIX" != "" ] && version_tag=${IMAGE_VERSION_TAG/$IMAGE_VERSION_TAG_PREFIX/}
+    [[ "$IMAGE_VERSION_TAG_PREFIX" != "" ]] && version_tag=${IMAGE_VERSION_TAG/$IMAGE_VERSION_TAG_PREFIX/}
 
     existing_image=$(docker images -q $DOCKERHUB_NAMESPACE/$service)
 
     # Only rebuild images if they do not exist or rebuild is forced.
-    if [ "$existing_image" == "" ] || [ "$FORCE_IMAGE_BUILD" != "" ]; then
+    if [[ "$existing_image" == "" ]] || [[ "$FORCE_IMAGE_BUILD" != "" ]]; then
       echo "==> Building \"$service\" image from file $file for service \"$DOCKERHUB_NAMESPACE/$service\""
-      docker build -f $file -t $DOCKERHUB_NAMESPACE/$service .
+      docker build -f $file -t $DOCKERHUB_NAMESPACE/$service --build-arg CLI_IMAGE=${CLI_IMAGE} .
     fi
 
     # Tag images with 'edge' tag and push.
-    echo "==> Tagged and pushed \"$service\" image to $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE"
-    docker tag $DOCKERHUB_NAMESPACE/$service $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
-    docker push $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
+    if [[ "$IMAGE_TAG_EDGE" != "" ]]; then
+      echo "==> Tagged and pushed \"$service\" image to $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE"
+      docker tag $DOCKERHUB_NAMESPACE/$service $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
+      docker push $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
+    fi
 
     # Tag images with version tag, if provided, and push.
-    if [ "$version_tag" != "" ]; then
-      echo "==> Tagging and pushing \"$service\" image to $DOCKERHUB_NAMESPACE/$service:$version_tag"
-      docker tag $DOCKERHUB_NAMESPACE/$service $DOCKERHUB_NAMESPACE/$service:$version_tag
-      docker push $DOCKERHUB_NAMESPACE/$service:$version_tag
+    if [[ "$version_tag" != "" ]]; then
+      CI_IMAGE_NAME=${CI_REGISTRY}/${DOCKERHUB_NAMESPACE}/$service;
+      echo "==> Tagging and pushing \"$service\" image to $CI_IMAGE_NAME:$version_tag"
+      docker tag $DOCKERHUB_NAMESPACE/$service $CI_IMAGE_NAME:$version_tag
+      docker push $CI_IMAGE_NAME:$version_tag
     fi
 done
